926f94c7fe436099c39f49c60689c9e0d5de6f16
SRDewan <shaurya.dewan@students.iiit.ac.in>
2023-04-20 04:25:01 +0530

    Few minor bug fixes, adding results and models for few experiments, also added visualizations of features.

--- models/gtsrb_cnn.py
+++ models/gtsrb_cnn.py
@@ -12,17 +12,19 @@ class GTSRB_CNN(nn.Module):
         # self.feature.add_module("f_bn1", nn.BatchNorm2d(96))
         self.feature.add_module("f_relu1", nn.ReLU(True))
         self.feature.add_module("f_pool1", nn.MaxPool2d(2, 2))
+        self.feature.add_module("f_drop1", nn.Dropout2d())
 
         self.feature.add_module("f_conv2", nn.Conv2d(96, 144, kernel_size = 3))
         # self.feature.add_module("f_bn2", nn.BatchNorm2d(144))
         self.feature.add_module("f_relu2", nn.ReLU(True))
         self.feature.add_module("f_pool2", nn.MaxPool2d(2, 2))
+        self.feature.add_module("f_drop2", nn.Dropout2d())
 
         self.feature.add_module("f_conv3", nn.Conv2d(144, 256, kernel_size = 5))
         # self.feature.add_module("f_bn3", nn.BatchNorm2d(256))
         self.feature.add_module("f_relu3", nn.ReLU(True))
         self.feature.add_module("f_pool3", nn.MaxPool2d(2, 2))
-        # self.feature.add_module("f_drop1", nn.Dropout2d())
+        self.feature.add_module("f_drop3", nn.Dropout2d())
         self.feature.add_module("f_flatten", nn.Flatten())
 
 
@@ -30,7 +32,7 @@ class GTSRB_CNN(nn.Module):
         self.class_classifier.add_module("c_fc1", nn.Linear(256 * 3 * 3, 512))
         # self.class_classifier.add_module("c_bn1", nn.BatchNorm1d(512))
         self.class_classifier.add_module("c_relu1", nn.ReLU(True))
-        # self.class_classifier.add_module("c_drop1", nn.Dropout1d())
+        self.class_classifier.add_module("c_drop1", nn.Dropout1d())
 
         self.class_classifier.add_module("c_fc2", nn.Linear(512, 43))
         # self.class_classifier.add_module("c_bn2", nn.BatchNorm1d(43))
@@ -41,12 +43,12 @@ class GTSRB_CNN(nn.Module):
         self.domain_classifier.add_module("d_fc1", nn.Linear(256 * 3 * 3, 1024))
         # self.domain_classifier.add_module("d_bn1", nn.BatchNorm1d(1024))
         self.domain_classifier.add_module("d_relu1", nn.ReLU(True))
-        # self.domain_classifier.add_module("d_drop1", nn.Dropout1d())
+        self.domain_classifier.add_module("d_drop1", nn.Dropout1d())
 
         self.domain_classifier.add_module("d_fc2", nn.Linear(1024, 1024))
         # self.domain_classifier.add_module("d_bn2", nn.BatchNorm1d(1024))
         self.domain_classifier.add_module("d_relu2", nn.ReLU(True))
-        # self.domain_classifier.add_module("d_drop2", nn.Dropout1d())
+        self.domain_classifier.add_module("d_drop2", nn.Dropout1d())
 
         self.domain_classifier.add_module("d_fc3", nn.Linear(1024, 1))
         # self.domain_classifier.add_module("d_bn3", nn.BatchNorm1d(1))
